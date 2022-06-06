ce078586fdcffd4af80c158767ea516037094a1a
terryleechen <73297091+terryleechen@users.noreply.github.com>
2022-06-05 18:09:40 -0600

    update login page style and work on employee customer page

--- Explore/Customer.Designer.cs
+++ Explore/Customer.Designer.cs
@@ -28,35 +28,212 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.first_name = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.customer_firstname = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.customer_lastname = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.customer_driver_license = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button_add = new System.Windows.Forms.Button();
+            this.customer_edit = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel5.SuspendLayout();
             this.SuspendLayout();
             // 
-            // first_name
+            // panel1
             // 
-            this.first_name.AutoSize = true;
-            this.first_name.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.first_name.ForeColor = System.Drawing.Color.SeaGreen;
-            this.first_name.Location = new System.Drawing.Point(70, 59);
-            this.first_name.Name = "first_name";
-            this.first_name.Size = new System.Drawing.Size(106, 24);
-            this.first_name.TabIndex = 0;
-            this.first_name.Text = "First Name:";
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.customer_firstname);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.ForeColor = System.Drawing.Color.SeaGreen;
+            this.panel1.Location = new System.Drawing.Point(23, 97);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(330, 51);
+            this.panel1.TabIndex = 0;
             // 
-            // Home
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "First Name:";
+            // 
+            // customer_firstname
+            // 
+            this.customer_firstname.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.customer_firstname.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.customer_firstname.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.customer_firstname.ForeColor = System.Drawing.Color.SeaGreen;
+            this.customer_firstname.Location = new System.Drawing.Point(106, 9);
+            this.customer_firstname.Name = "customer_firstname";
+            this.customer_firstname.Size = new System.Drawing.Size(150, 22);
+            this.customer_firstname.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.SeaGreen;
+            this.panel2.Location = new System.Drawing.Point(106, 36);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(150, 1);
+            this.panel2.TabIndex = 3;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.panel4);
+            this.panel3.Controls.Add(this.customer_lastname);
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.ForeColor = System.Drawing.Color.SeaGreen;
+            this.panel3.Location = new System.Drawing.Point(23, 179);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(330, 51);
+            this.panel3.TabIndex = 4;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.SeaGreen;
+            this.panel4.Location = new System.Drawing.Point(106, 36);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(150, 1);
+            this.panel4.TabIndex = 3;
+            // 
+            // customer_lastname
+            // 
+            this.customer_lastname.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.customer_lastname.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.customer_lastname.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.customer_lastname.ForeColor = System.Drawing.Color.SeaGreen;
+            this.customer_lastname.Location = new System.Drawing.Point(106, 9);
+            this.customer_lastname.Name = "customer_lastname";
+            this.customer_lastname.Size = new System.Drawing.Size(150, 22);
+            this.customer_lastname.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(3, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 24);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Last Name:";
+            // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.panel6);
+            this.panel5.Controls.Add(this.customer_driver_license);
+            this.panel5.Controls.Add(this.label3);
+            this.panel5.ForeColor = System.Drawing.Color.SeaGreen;
+            this.panel5.Location = new System.Drawing.Point(23, 256);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(330, 51);
+            this.panel5.TabIndex = 5;
+            // 
+            // panel6
+            // 
+            this.panel6.BackColor = System.Drawing.Color.SeaGreen;
+            this.panel6.Location = new System.Drawing.Point(133, 36);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(125, 1);
+            this.panel6.TabIndex = 3;
+            // 
+            // customer_driver_license
+            // 
+            this.customer_driver_license.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.customer_driver_license.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.customer_driver_license.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.customer_driver_license.ForeColor = System.Drawing.Color.SeaGreen;
+            this.customer_driver_license.Location = new System.Drawing.Point(133, 9);
+            this.customer_driver_license.Name = "customer_driver_license";
+            this.customer_driver_license.Size = new System.Drawing.Size(125, 22);
+            this.customer_driver_license.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(3, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(135, 24);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Driver License:";
+            // 
+            // button_add
+            // 
+            this.button_add.BackColor = System.Drawing.Color.Transparent;
+            this.button_add.FlatAppearance.BorderColor = System.Drawing.Color.SeaGreen;
+            this.button_add.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.button_add.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_add.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_add.ForeColor = System.Drawing.Color.SeaGreen;
+            this.button_add.Location = new System.Drawing.Point(23, 26);
+            this.button_add.Name = "button_add";
+            this.button_add.Size = new System.Drawing.Size(82, 40);
+            this.button_add.TabIndex = 6;
+            this.button_add.Text = "Add";
+            this.button_add.UseVisualStyleBackColor = false;
+            // 
+            // customer_edit
+            // 
+            this.customer_edit.BackColor = System.Drawing.Color.Transparent;
+            this.customer_edit.FlatAppearance.BorderColor = System.Drawing.Color.SeaGreen;
+            this.customer_edit.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.customer_edit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.customer_edit.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.customer_edit.ForeColor = System.Drawing.Color.SeaGreen;
+            this.customer_edit.Location = new System.Drawing.Point(129, 26);
+            this.customer_edit.Name = "customer_edit";
+            this.customer_edit.Size = new System.Drawing.Size(82, 40);
+            this.customer_edit.TabIndex = 7;
+            this.customer_edit.Text = "Edit";
+            this.customer_edit.UseVisualStyleBackColor = false;
+            // 
+            // Customer
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
-            this.Controls.Add(this.first_name);
-            this.Name = "Home";
+            this.Controls.Add(this.customer_edit);
+            this.Controls.Add(this.button_add);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel1);
+            this.Name = "Customer";
             this.Size = new System.Drawing.Size(790, 460);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel5.ResumeLayout(false);
+            this.panel5.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Label first_name;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox customer_firstname;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.TextBox customer_lastname;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.TextBox customer_driver_license;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button_add;
+        private System.Windows.Forms.Button customer_edit;
     }
 }
