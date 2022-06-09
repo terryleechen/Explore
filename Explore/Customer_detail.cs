8f7db5a2b6a275196e254098242225c74cc2d454
Josh Coss <josh.coss@gmail.com>
2022-06-09 10:50:10 -0600

    Customer Detail Edit
    Employee can now edit all customer details except the customer ID number (which we do not want them to be able to edit)

--- Explore/Customer_detail.cs
+++ Explore/Customer_detail.cs
@@ -13,9 +13,11 @@ namespace Explore
     public partial class Customer_detail : UserControl
     {
         private Employee_dashboard employee_dashboard;
+        private SQL sql;
         public Customer_detail()
         {
             InitializeComponent();
+            this.sql = new SQL();
         }
 
         public void UpdateView(String CID, String First_Name, String Last_Name, String Driver_License,
@@ -25,45 +27,23 @@ namespace Explore
             customer_save.Enabled = false;
             this.ReadOnly(true);
 
-            this.CID.Text = CID;
-            this.FirstName.Text = First_Name;
-            this.LastName.Text = Last_Name;
-            this.Driver_License.Text = Driver_License;
-            this.Address_1.Text = Address_1;
-            this.Address_2.Text = Address_2;
-            this.City.Text = City;
-            this.PostalCode.Text = Postal_Code;
-            this.Email.Text = Email;
-
-            if (Membership == "Y")
-            {
-                this.Membership.Checked = true;
-            } else
-            {
-                this.Membership.Checked = false;
-            }
-
-            char[] parseChars = DOB.ToCharArray();
-            DateTime DateTimeDOB = DateTime.Parse(parseChars[0].ToString() + 
-                parseChars[1] + parseChars[2] + parseChars[3] + "-" +
-                parseChars[5] + parseChars[6] + '-' + parseChars[8] + parseChars[9]);
-            this.DOB.Value = DateTimeDOB;
-
-            this.Province.SelectedItem = Province;
-
-            if (Gender == "M")
-            {
-                this.M.Checked = true; this.M.Enabled = true;
-            } else if (Gender == "F")
-            {
-                this.F.Checked = true; this.F.Enabled = true;
-            } else if (Gender == "N")
-            {
-                this.N.Checked = true; this.N.Enabled = true;
-            }
+            this.Populate(CID, First_Name, Last_Name, Driver_License, Address_1,
+                Address_2, City, Postal_Code, Email, Membership, DOB, Province, Gender);
         }
 
-        public void UpdateView(string Phone_Number)
+        public void Edit(String CID, String First_Name, String Last_Name, String Driver_License,
+            String Address_1, String Address_2, String City, String Postal_Code, String Email,
+            String Membership, String DOB, String Province, String Gender)
+        {
+            customer_save.Enabled = true;
+            this.ReadOnly(false);
+            this.CID.ReadOnly = true;
+
+            this.Populate(CID, First_Name, Last_Name, Driver_License, Address_1,
+                Address_2, City, Postal_Code, Email, Membership, DOB, Province, Gender);
+        }
+
+        public void UpdatePhoneNumber(string Phone_Number)
         {
             this.PhoneNumber.Text = Phone_Number;
         }
@@ -103,7 +83,77 @@ namespace Explore
 
         private void Button_save_click(object sender, EventArgs e)
         {
+            String mem = "";
+            String gen = "";
 
+            if (this.Membership.Checked) { mem = "Y"; }
+            else { mem = "N"; }
+
+            if (this.M.Checked) { gen = "M"; }
+            else if (this.F.Checked) { gen = "F"; }
+            else if (this.N.Checked) { gen = "N"; }
+
+            String date = "";
+            char[] birthdate = this.DOB.Value.ToString().ToCharArray();
+            date = birthdate[0].ToString() + birthdate[1] + birthdate[2] + birthdate[3] +
+                 "/" + birthdate[5] + birthdate[6] + "/" + birthdate[8] + birthdate[9];
+
+            this.sql.Update("Update Customer " +
+                "Set First_Name = '" + this.FirstName.Text + "', " +
+                "Last_Name = '" + this.LastName.Text + "', " +
+                "Driver_License = '" + this.Driver_License.Text + "', " +
+                "Address_1 = '" + this.Address_1.Text + "', " +
+                "Membership = '" + mem + "', " +
+                "City = '" + this.City.Text + "', " +
+                "Postal_code = '" + this.PostalCode.Text + "', " +
+                "Email = '" + this.Email.Text + "', " +
+                "Gender = '" + gen + "', " +
+                "Province = '" + this.Province.SelectedItem + "', " +
+                "DOB = '" + date + "', " +
+                "Address_2 = '" + this.Address_2.Text + "' " +
+                "Where CID = '" + this.CID.Text + "'");
+            this.sql.Update("Update Customer_Phone " +
+                "Set Phone_Number = '" + this.PhoneNumber.Text + "' " +
+                "Where CID = '" + this.CID.Text + "'");
+
+            this.employee_dashboard.Get_Customer().Show();
+            this.Hide();
+        }
+
+        private void Populate(String CID, String First_Name, String Last_Name, String Driver_License,
+            String Address_1, String Address_2, String City, String Postal_Code, String Email,
+            String Membership, String DOB, String Province, String Gender)
+        {
+            this.CID.Text = CID;
+            this.FirstName.Text = First_Name;
+            this.LastName.Text = Last_Name;
+            this.Driver_License.Text = Driver_License;
+            this.Address_1.Text = Address_1;
+            this.Address_2.Text = Address_2;
+            this.City.Text = City;
+            this.PostalCode.Text = Postal_Code;
+            this.Email.Text = Email;
+
+            if (Membership == "Y")
+            {
+                this.Membership.Checked = true;
+            }
+            else
+            {
+                this.Membership.Checked = false;
+            }
+
+            char[] parseChars = DOB.ToCharArray();
+            DateTime DateTimeDOB = DateTime.Parse(parseChars[0].ToString() +
+                parseChars[1] + parseChars[2] + parseChars[3] + "-" +
+                parseChars[5] + parseChars[6] + '-' + parseChars[8] + parseChars[9]);
+            this.DOB.Value = DateTimeDOB;
+
+            this.Province.SelectedItem = Province;
+
+            if (Gender == "M") { this.M.Checked = true; this.M.Enabled = true; }
+            else if (Gender == "F") { this.F.Checked = true; this.F.Enabled = true; }
+            else if (Gender == "N") { this.N.Checked = true; this.N.Enabled = true; }
         }
 
     }
