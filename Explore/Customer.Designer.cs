e7bdabc944da4d17257727f8395844393ceeb272
Josh Coss <josh.coss@gmail.com>
2022-06-19 12:26:37 -0600

    Added Comments, Changed Layout of Customer.cs

--- Explore/Customer.Designer.cs
+++ Explore/Customer.Designer.cs
@@ -28,14 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.customer_firstname = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.panel4 = new System.Windows.Forms.Panel();
-            this.customer_lastname = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
             this.panel5 = new System.Windows.Forms.Panel();
             this.panel6 = new System.Windows.Forms.Panel();
             this.customer_driver_license = new System.Windows.Forms.TextBox();
@@ -43,93 +35,9 @@
             this.button_add = new System.Windows.Forms.Button();
             this.customer_edit = new System.Windows.Forms.Button();
             this.customer_view = new System.Windows.Forms.Button();
-            this.panel1.SuspendLayout();
-            this.panel3.SuspendLayout();
+            this.label1 = new System.Windows.Forms.Label();
             this.panel5.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // panel1
-            // 
-            this.panel1.Controls.Add(this.panel2);
-            this.panel1.Controls.Add(this.customer_firstname);
-            this.panel1.Controls.Add(this.label1);
-            this.panel1.ForeColor = System.Drawing.Color.SeaGreen;
-            this.panel1.Location = new System.Drawing.Point(23, 97);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(330, 51);
-            this.panel1.TabIndex = 0;
-            // 
-            // panel2
-            // 
-            this.panel2.BackColor = System.Drawing.Color.SeaShell;
-            this.panel2.Location = new System.Drawing.Point(106, 36);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(150, 1);
-            this.panel2.TabIndex = 3;
-            // 
-            // customer_firstname
-            // 
-            this.customer_firstname.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
-            this.customer_firstname.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.customer_firstname.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.customer_firstname.ForeColor = System.Drawing.Color.SeaShell;
-            this.customer_firstname.Location = new System.Drawing.Point(106, 9);
-            this.customer_firstname.Name = "customer_firstname";
-            this.customer_firstname.Size = new System.Drawing.Size(150, 22);
-            this.customer_firstname.TabIndex = 2;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.SeaShell;
-            this.label1.Location = new System.Drawing.Point(3, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(106, 24);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "First Name:";
-            // 
-            // panel3
-            // 
-            this.panel3.Controls.Add(this.panel4);
-            this.panel3.Controls.Add(this.customer_lastname);
-            this.panel3.Controls.Add(this.label2);
-            this.panel3.ForeColor = System.Drawing.Color.SeaGreen;
-            this.panel3.Location = new System.Drawing.Point(23, 179);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(330, 51);
-            this.panel3.TabIndex = 4;
-            // 
-            // panel4
-            // 
-            this.panel4.BackColor = System.Drawing.Color.SeaShell;
-            this.panel4.ForeColor = System.Drawing.Color.SeaShell;
-            this.panel4.Location = new System.Drawing.Point(106, 36);
-            this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(150, 1);
-            this.panel4.TabIndex = 3;
-            // 
-            // customer_lastname
-            // 
-            this.customer_lastname.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
-            this.customer_lastname.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.customer_lastname.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.customer_lastname.ForeColor = System.Drawing.Color.SeaShell;
-            this.customer_lastname.Location = new System.Drawing.Point(106, 9);
-            this.customer_lastname.Name = "customer_lastname";
-            this.customer_lastname.Size = new System.Drawing.Size(150, 22);
-            this.customer_lastname.TabIndex = 2;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.Color.SeaShell;
-            this.label2.Location = new System.Drawing.Point(3, 9);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(104, 24);
-            this.label2.TabIndex = 0;
-            this.label2.Text = "Last Name:";
             // 
             // panel5
             // 
@@ -137,18 +45,18 @@
             this.panel5.Controls.Add(this.customer_driver_license);
             this.panel5.Controls.Add(this.label3);
             this.panel5.ForeColor = System.Drawing.Color.SeaGreen;
-            this.panel5.Location = new System.Drawing.Point(23, 256);
+            this.panel5.Location = new System.Drawing.Point(23, 44);
             this.panel5.Name = "panel5";
-            this.panel5.Size = new System.Drawing.Size(330, 51);
+            this.panel5.Size = new System.Drawing.Size(500, 98);
             this.panel5.TabIndex = 5;
             // 
             // panel6
             // 
             this.panel6.BackColor = System.Drawing.Color.SeaShell;
             this.panel6.ForeColor = System.Drawing.Color.SeaShell;
-            this.panel6.Location = new System.Drawing.Point(133, 36);
+            this.panel6.Location = new System.Drawing.Point(60, 87);
             this.panel6.Name = "panel6";
-            this.panel6.Size = new System.Drawing.Size(125, 1);
+            this.panel6.Size = new System.Drawing.Size(212, 1);
             this.panel6.TabIndex = 3;
             // 
             // customer_driver_license
@@ -157,9 +65,10 @@
             this.customer_driver_license.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.customer_driver_license.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.customer_driver_license.ForeColor = System.Drawing.Color.SeaShell;
-            this.customer_driver_license.Location = new System.Drawing.Point(133, 9);
+            this.customer_driver_license.Location = new System.Drawing.Point(60, 60);
+            this.customer_driver_license.MaxLength = 9;
             this.customer_driver_license.Name = "customer_driver_license";
-            this.customer_driver_license.Size = new System.Drawing.Size(125, 22);
+            this.customer_driver_license.Size = new System.Drawing.Size(212, 22);
             this.customer_driver_license.TabIndex = 2;
             // 
             // label3
@@ -169,9 +78,10 @@
             this.label3.ForeColor = System.Drawing.Color.SeaShell;
             this.label3.Location = new System.Drawing.Point(3, 9);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(135, 24);
+            this.label3.Size = new System.Drawing.Size(383, 48);
             this.label3.TabIndex = 0;
-            this.label3.Text = "Driver License:";
+            this.label3.Text = "To View or Edit Customer\'s Information,\r\nPlease Enter Their Driver\'s License Numb" +
+    "er:";
             // 
             // button_add
             // 
@@ -181,7 +91,7 @@
             this.button_add.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button_add.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button_add.ForeColor = System.Drawing.Color.SeaShell;
-            this.button_add.Location = new System.Drawing.Point(129, 32);
+            this.button_add.Location = new System.Drawing.Point(23, 300);
             this.button_add.Name = "button_add";
             this.button_add.Size = new System.Drawing.Size(82, 40);
             this.button_add.TabIndex = 6;
@@ -196,7 +106,7 @@
             this.customer_edit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.customer_edit.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.customer_edit.ForeColor = System.Drawing.Color.SeaShell;
-            this.customer_edit.Location = new System.Drawing.Point(235, 32);
+            this.customer_edit.Location = new System.Drawing.Point(123, 148);
             this.customer_edit.Name = "customer_edit";
             this.customer_edit.Size = new System.Drawing.Size(82, 40);
             this.customer_edit.TabIndex = 7;
@@ -211,8 +121,7 @@
             this.customer_view.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.customer_view.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.customer_view.ForeColor = System.Drawing.Color.SeaShell;
-            this.customer_view.Location = new System.Drawing.Point(23, 32);
-
+            this.customer_view.Location = new System.Drawing.Point(23, 148);
             this.customer_view.Name = "customer_view";
             this.customer_view.Size = new System.Drawing.Size(82, 40);
             this.customer_view.TabIndex = 8;
@@ -220,39 +129,37 @@
             this.customer_view.UseVisualStyleBackColor = false;
             this.customer_view.Click += new System.EventHandler(this.Button_view_click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.SeaShell;
+            this.label1.Location = new System.Drawing.Point(26, 240);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(381, 48);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "To Add A New Customer To The Database, \r\nClick The Add Button";
+            // 
             // Customer
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.customer_view);
             this.Controls.Add(this.customer_edit);
             this.Controls.Add(this.button_add);
             this.Controls.Add(this.panel5);
-            this.Controls.Add(this.panel3);
-            this.Controls.Add(this.panel1);
             this.Name = "Customer";
             this.Size = new System.Drawing.Size(790, 460);
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
-            this.panel3.ResumeLayout(false);
-            this.panel3.PerformLayout();
             this.panel5.ResumeLayout(false);
             this.panel5.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox customer_firstname;
-        private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Panel panel3;
-        private System.Windows.Forms.Panel panel4;
-        private System.Windows.Forms.TextBox customer_lastname;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Panel panel5;
         private System.Windows.Forms.Panel panel6;
         private System.Windows.Forms.TextBox customer_driver_license;
@@ -260,5 +167,6 @@
         private System.Windows.Forms.Button button_add;
         private System.Windows.Forms.Button customer_edit;
         private System.Windows.Forms.Button customer_view;
+        private System.Windows.Forms.Label label1;
     }
 }
--- Explore/Customer.cs
+++ Explore/Customer.cs
@@ -7,19 +7,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Text.RegularExpressions;
 
 namespace Explore
 {
-    /*
-     * This is the customer panel in employee dashboard
-     * 
-     * Author: Josh Coss
-     */
+    /// <summary>
+    /// Customer detail landing page. From here the user chooses whether they want to view,
+    /// edit, or add customer information to the database. If view or edit is chosen, the
+    /// class queries the database based on the customer's driver's license number.
+    /// </summary>
     public partial class Customer : UserControl
     {
         private Customer_detail customer_detail;
         private SQL sql;
 
+        /// <summary>
+        /// Initializes the page
+        /// </summary>
+        /// <param name="customer_detail"></param>
         public Customer(Customer_detail customer_detail)
         {
             InitializeComponent();
@@ -27,28 +32,42 @@ namespace Explore
             this.sql = new SQL();
         }
 
+        /// <summary>
+        /// Brings up the customer detail page in the Add state
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void Button_add_click(object sender, EventArgs e)
         {
             this.customer_detail.AddCustomer();
             this.customer_detail.Show();
         }
 
+        /// <summary>
+        /// Queries the database for the customer's information bassed on the driver's license
+        /// number in order to edit
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void Button_edit_click(object sender, EventArgs e)
         {
             String driver_license = customer_driver_license.Text;
             bool check = false;
             this.customer_driver_license.Clear();
 
-            if (driver_license == null)
+            // Makes sure the driver's license number is valid
+            if (!ValidateDriverLicense(driver_license))
             {
                 MessageBox.Show("Please enter a valid driver's license number");
-                this.sql.Close();
             }
             else
             {
+                // Query the database for all customer driver's licenses
                 this.sql.Query("select Driver_License from Customer");
                 while (this.sql.Reader().Read())
                 {
+                    // If Driver's License is in the selected relation, exit the loop and set
+                    // check to true, otherwise show error message
                     if (this.sql.Reader()["Driver_License"].ToString().Equals(driver_license))
                     {
                         check = true;
@@ -56,7 +75,13 @@ namespace Explore
                         break;
                     }
                 }
-
+                if (check == false)
+                {
+                    MessageBox.Show("No such customer exists");
+                    this.sql.Close();
+                }
+                // Query the database for all information where the driver's license equals Driver_License.
+                // Clear the table, insert the information into the page, and show the Customer_Detail page.
                 if (check == true)
                 {
                     this.sql.Query("select * from Customer where Driver_License = " + driver_license);
@@ -87,42 +112,54 @@ namespace Explore
                     }
                     this.customer_detail.Show();
                 }
-                else
-                {
-                    MessageBox.Show("No such customer exists");
-
-                }
+                
                 this.sql.Close();
             }
         }
 
+        /// <summary>
+        /// Queries the database for the customer's information bassed on the driver's license
+        /// number in order to view
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void Button_view_click(object sender, EventArgs e)
         {
             String driver_license = customer_driver_license.Text;
             bool check = false;
             this.customer_driver_license.Clear();
-            
 
-            if (driver_license == null)
+
+            // Makes sure the driver's license number is valid
+            if (!ValidateDriverLicense(driver_license))
             {
                 MessageBox.Show("Please enter a valid driver's license number");
-                this.sql.Close();
-            } 
+            }
             else
             {
+                // Query the database for all customer driver's licenses
                 this.sql.Query("select Driver_License from Customer");
                 while (this.sql.Reader().Read())
                 {
+                    // If Driver's License is in the selected relation, exit the loop and set
+                    // check to true, otherwise show error message
                     if (this.sql.Reader()["Driver_License"].ToString().Equals(driver_license))
                     {
                         check = true;
                         this.sql.Close();
                         break;
-                    } 
+                    }
                 }
-                
+                if (check == false)
+                {
+                    MessageBox.Show("No such customer exists");
+                    this.sql.Close();
+                }
+
                 if (check == true)
                 {
+                    // Query the database for all information where the driver's license equals Driver_License.
+                    // Clear the table, insert the information into the page, and show the Customer_Detail page.
                     this.sql.Query("select * from Customer where Driver_License = " + driver_license);
                     this.customer_detail.Clear();
                     while (this.sql.Reader().Read())
@@ -151,14 +188,24 @@ namespace Explore
                     }
                     this.customer_detail.Show();
                 } 
-                else
-                {
-                    MessageBox.Show("No such customer exists");
-                    
-                }
+
                 this.sql.Close();
                 
             }
+        }
+
+        /// <summary>
+        /// Validates Driver's License
+        /// </summary>
+        /// <param name="dl">String with potential driver's license</param>
+        /// <returns>bool true/false</returns>
+        public static bool ValidateDriverLicense(string dl)
+        {
+            string pattern = @"^\d+$";
+            Regex reg = new Regex(pattern);
+            if (!reg.IsMatch(dl) || dl.Length < 9)
+                return false;
+            return true;
         }
     }
 }
